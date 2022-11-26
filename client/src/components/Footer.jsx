64f457f2ad66d32d0fdcaba949ede981d8d5b1e6
Jzavarelli <jlzavarelli@gmail.com>
2022-11-26 14:55:44 -0700

    update-jace-index-server

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,20 @@
+const Footer = () => {
+
+    return (
+        <div>
+            <h2>Footer</h2>
+            <p><em> Jace Zavarelli, n76t836</em></p>
+            <p>My role in Group 5 is to run and work on the functions of the FrontEnd within the Backend:</p>
+            <ul>
+                <li>server/index.js - Build an addition to the create tab that will add each of the whitelisting details automatically to the columns.</li>
+                <li>server/index.js - Build a new type of update for specifically blacklisting members and volunteers, will be done in collab with the frontend.</li>
+                <li>server/index.js - Build a notification system to alert if someone is automatically blacklisted due to conflicts with their signup information.</li>
+                <li>Document - Record the main work each member commits to the repo, while also connecting details with other groups for our feature.</li>
+                <li>Document - Write a detailed write up of the function of our feature, while also showing how it functions within the wep app.</li>
+            </ul>
+            <p>Github: <a href="https://github.com/Jzavarelli/CSCI_Group_5_Test_Repo">https://github.com/Jzavarelli/CSCI_Group_5_Test_Repo</a></p>
+        </div>
+    )
+}
+
+export default Footer
